e6b301a88c34dde3c40bcd366498dcb21016a1d6
Rasmus <rasmusbamse@hotmail.com>
2020-04-27 21:18:16 +0200

    Read metadata.json directly without writing to disk.

--- parsecontents.py
+++ parsecontents.py
@@ -7,43 +7,28 @@ import json
 def get(repo="hbb1.oscwii.org"):
     u = requests.get("https://" + repo + "/metadata.json")
 
-    with open("metadata.json", "wb") as f:  # I want to change this, as this writes to disk
-        f.write(u.content)
-
-    with open('metadata.json') as f:
-        data = json.load(f)
-        for key in data.keys():
-            print(key)
-
-    os.remove("metadata.json")
+    data = json.loads(u.content)
+    for key in data.keys():
+        print(key)
 
 
 def query(term, repo="hbb1.oscwii.org"):
     print("Searching for package on " + repo + "..")
     u = requests.get("https://" + repo + "/metadata.json")
 
-    with open("metadata.json", "wb") as f:  # I want to change this, as this writes to disk
-        f.write(u.content)
+    data = json.loads(u.content)
 
-    with open('metadata.json') as f:
-        data = json.load(f)
+    found = "false"
 
-        found = "false"
+    for key in data.keys():
+        if key == term:
+            found = "true"
 
-        for key in data.keys():
-            if key == term:
-                found = "true"
-
-        if found == "true":
-            print("Found package!")
-        else:
-            print('Could not find "' + term + '" on the repository. :(')
-            return False
-
-    try:
-        os.remove("metadata.json")
-    except Exception:
-        print("[Error P001] Cannot delete metadata.json. Please delete it yourself. D:")
+    if found == "true":
+        print("Found package!")
+    else:
+        print('Could not find "' + term + '" on the repository. :(')
+        return False
 
     return True
 
@@ -52,11 +37,6 @@ def get_list(repo="hbb1.oscwii.org"):
     print("Getting list of all packages from " + repo + "..")
     u = requests.get("https://" + repo + "/metadata.json")
 
-    with open("metadata.json", "wb") as f:  # I want to change this, as this writes to disk
-        f.write(u.content)
+    data = json.loads(u.content)
 
-    with open('metadata.json') as f:
-        data = json.load(f)
-
-    os.remove("metadata.json")
     return data
