4c281545428ade4ba32d3bd81b09bd0100259a8d
dhtdht020 <dhtdht020@gmail.com>
2020-05-31 14:21:29 +0300

    Experimental check if PyInstaller is used

--- updater.py
+++ updater.py
@@ -1,7 +1,23 @@
 from halo import Halo
+import os
+import sys
+
+
+def is_frozen():
+    if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+        # Packaged with PyInstaller.
+        return True
+    else:
+        # Running in a normal Python script, not packaged by PyInstaller.
+        return False
 
 
 def init_update():
+    if is_frozen() is True:
+        print('Packaged with PyInstaller.')
+    else:
+        print('Running in a normal Python script, not packaged by PyInstaller.')
+
     print("Get the latest version from https://github.com/dhtdht020/osc-dl/")
 
     with Halo(text="Checking for updates..", color="yellow", text_color="yellow"):
