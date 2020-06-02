18aaceeb716a5f8cda405f49c626efe90c2d4716
dhtdht020 <dhtdht020@gmail.com>
2020-06-02 19:51:09 +0300

    Added option to get dictionary for metadata

--- metadata.py
+++ metadata.py
@@ -2,6 +2,7 @@ import parsecontents
 import requests
 import lxml.etree
 
+
 GREEN = '\033[92m'
 FAIL = '\033[91m'
 
@@ -86,3 +87,66 @@ def get(app_name, type=None, repo="hbb1.oscwii.org"):
     print("Version: " + version)
     print("Description: " + short_description)
     print("============================================\n")
+
+
+# experimental, for use by xosc-dl
+def icon(app_name, repo="hbb1.oscwii.org"):
+    icon = "https://" + repo+ "/hbb/"+ app_name + ".png"
+    return icon
+
+
+def dictionary(app_name, repo="hbb1.oscwii.org"):
+
+    xml = requests.get("https://" + repo + "/unzipped_apps/" + app_name + "/apps/" + app_name + "/meta.xml").text
+
+    # remove unicode declaration
+    xml = xml.split("\n", 1)[1]
+
+    # get information from XML
+    root = lxml.etree.fromstring(xml)
+
+    try:
+        display_name = root.find('name').text
+    except Exception:
+        display_name = app_name
+
+    try:
+        developer = root.find('coder').text
+    except Exception:
+        developer = "Unknown"
+
+    try:
+        version = root.find('version').text
+    except Exception:
+        version = "Unknown"
+
+    try:
+        short_description = root.find('short_description').text
+    except Exception:
+        short_description = "Unknown"
+
+    try:
+        long_description = root.find('long_description').text
+    except Exception:
+        long_description = "Unknown"
+
+    try:
+        release_date = root.find('release_date').text
+    except Exception:
+        release_date = "Unknown"
+
+    try:
+        contributors = root.find('contributors').text
+    except Exception:
+        contributors = "Unknown"
+
+    meta = {"display_name": display_name,
+            "coder": developer,
+            "version": version,
+            "short_description": short_description,
+            "long_description": long_description,
+            "release_date": release_date,
+            "contributors": contributors
+            }
+
+    return meta
