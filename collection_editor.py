bbc9c95af83144017bd1f27f50b608fd63a6b565
dhtdht020 <dhtdht020@gmail.com>
2020-10-06 10:37:17 +0300

    More work on Collection Editor

--- collection_editor.py
+++ collection_editor.py
@@ -12,11 +12,11 @@ from functools import partial
 
 import requests
 import pyperclip
-from PySide2 import QtGui
+from PySide2 import QtGui, QtCore, QtWidgets
 from PySide2.QtCore import Qt
 from PySide2.QtGui import QIcon
 from PySide2.QtWidgets import QApplication, QMainWindow, QInputDialog, QLineEdit, QMessageBox, QSplashScreen, QWidget, \
-    QMdiSubWindow
+    QMdiSubWindow, QVBoxLayout
 
 import download
 import gui.ui_cedit
@@ -71,19 +71,36 @@ class MainWindow(gui.ui_cedit.Ui_MainWindow, QMainWindow):
         #sub_window.setWidget(QWidget(CollectionWindow))
         #self.ui.mdiArea.addSubWindow(sub_window)
 
+        self.create_collection_contents_pane()
+        self.create_host_contents_pane()
 
-# collection window
-class CollectionWindow(gui.ceditor_mdi.ui_collection.Ui_CollectionMainForm, QWidget):
-    def __init__(self):
-        super(CollectionWindow, self).__init__()
-        self.collection_ui = gui.ui_cedit.Ui_MainWindow()
-        self.collection_ui.setupUi(self)
+    def create_collection_contents_pane(self):
+        collection_view = QMdiSubWindow()
+        widget = QtWidgets.QListWidget()
+        collection_view.setWidget(widget)
+        self.ui.mdiArea.addSubWindow(collection_view)
+        collection_view.setWindowTitle("Contents: Unnamed New Collection")
+        collection_view.show()
+        widget.addItem("WiiVNC")
+        widget.addItem("NewoEscape")
+        widget.addItem("wiibricker_9000")
+        widget.addItem("Achtung_Wii_Kurve")
+        widget.addItem("BootMiiConfigurationEditor")
 
-        # Set title and icon of window
-        #self.setWindowTitle(f"Open Shop Channel Collection Editor v{DISPLAY_VERSION}")
-        #app_icon = QIcon(resource_path("assets/gui/windowicon.png"))
-        #self.setWindowIcon(app_icon)
-
+    def create_host_contents_pane(self, host="hbb1.oscwii.org"):
+        collection_view = QMdiSubWindow()
+        layout = QVBoxLayout()
+        widget = QtWidgets.QListWidget()
+        button = QtWidgets.QPushButton()
+        layout.addWidget(widget)
+        layout.addWidget(button)
+        collection_view.setLayout(layout)
+        self.ui.mdiArea.addSubWindow(collection_view)
+        collection_view.setWindowTitle("Open Shop Channel")
+        collection_view.show()
+        self.applist = parsecontents.list(repo=host)
+        for i in self.applist:
+            widget.addItem(i)
 
 if __name__ == "__main__":
     app = QApplication()
