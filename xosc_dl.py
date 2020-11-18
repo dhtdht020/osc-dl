d6d4d48d93804450fda6ddcf75f3ca054cf73191
dhtdht020 <dhtdht020@gmail.com>
2020-11-18 18:27:51 +0200

    Updated splash

--- xosc_dl.py
+++ xosc_dl.py
@@ -5,7 +5,7 @@ from os import listdir
 from os.path import isfile, join
 
 import yaml
-# import sentry_sdk
+import sentry_sdk
 import os
 import re
 import socket
@@ -54,15 +54,14 @@ def resource_path(relative_path):
     return os.path.join(os.path.abspath("."), relative_path)
 
 
-# if updater.is_frozen():
-#    # Init sentry
-#    sentry_sdk.init(
-#        "https://619963fe9ec346e1b032fb19ea1632c8@o456896.ingest.sentry.io/5450395",
-#        traces_sample_rate=1.0
-#    )
-#
-#    with sentry_sdk.configure_scope() as scope:
-#        scope.set_tag("osc.release", VERSION)
+if updater.is_frozen():
+   # Init sentry
+   sentry_sdk.init(
+       "https://619963fe9ec346e1b032fb19ea1632c8@o456896.ingest.sentry.io/5450395",
+       traces_sample_rate=1.0
+   )
+   with sentry_sdk.configure_scope() as scope:
+       scope.set_tag("osc.release", VERSION)
 
 
 # G U I
