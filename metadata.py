1b763d597f3b7636d5d762cb7615a1db0277e77c
dhtdht020 <dhtdht020@gmail.com>
2021-04-16 14:36:07 +0300

    Fallback response when not connected to the internet

--- metadata.py
+++ metadata.py
@@ -5,7 +5,6 @@ import sys
 import requests
 import lxml.etree
 
-
 GREEN = '\033[92m'
 FAIL = '\033[91m'
 
@@ -20,7 +19,7 @@ def resource_path(relative_path):
 # Get icon of homebrew app. Should be updated to use API sooner or later!
 def icon(app_name, repo="hbb1.oscwii.org"):
     try:
-        request = requests.get("https://" + repo+ "/hbb/"+ app_name + ".png")
+        request = requests.get("https://" + repo + "/hbb/" + app_name + ".png")
         icon = request.content
         # If icon is not there
         if str(request.status_code) != "200":
@@ -43,22 +42,72 @@ def url(app_name, repo="hbb1.oscwii.org"):
 
 # Get JSON of specified packages from API
 def get_apps(host_name="primary", category="all", coder=None):
-    if category == "all":
-        json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages")
-        if coder is not None:
-            json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages?coder={coder}")
-    else:
-        json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages?category={category}")
+    try:
+        if category == "all":
+            json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages")
+            if coder is not None:
+                json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages?coder={coder}")
+        else:
+            json_req = requests.get(f"https://api.oscwii.org/v2/{host_name}/packages?category={category}")
 
-    if json_req.status_code != 200:
-        raise Exception("Cannot reach Open Shop Channel API.")
+        if json_req.status_code != 200:
+            raise Exception("Cannot reach Open Shop Channel API.")
+
+    except Exception as e:
+        # Return fake apps list with offline app
+        return json.loads('''
+        [{
+            "category": "demos", 
+            "coder": "dhtdht020", 
+            "contributors": "", 
+            "controllers": "", 
+            "display_name": "1 : You are not connected to the internet.", 
+            "downloads": 0, 
+            "extra_directories": [], 
+            "extracted": 0, 
+            "icon_url": "https://hbb1.oscwii.org/hbb/offline.png", 
+            "internal_name": "FakeAppOfflineError", 
+            "long_description": "Could not connect to the server. Please check your internet connection.", 
+            "package_type": "dol", 
+            "rating": "", 
+            "release_date": 1557464400, 
+            "shop_title_id": "", 
+            "shop_title_version": "", 
+            "short_description": "Please check your internet connection.", 
+            "updated": 1557464400, 
+            "version": "1", 
+            "zip_size": 0, 
+            "zip_url": "https://hbb1.oscwii.org/hbb/offline/offline.zip"
+        },
+        {
+            "category": "demos", 
+            "coder": "dhtdht020", 
+            "contributors": "", 
+            "controllers": "", 
+            "display_name": "2 : The server is potentially down.", 
+            "downloads": 0, 
+            "extra_directories": [], 
+            "extracted": 0, 
+            "icon_url": "https://hbb1.oscwii.org/hbb/offline.png", 
+            "internal_name": "FakeAppOfflineInfo", 
+            "long_description": "OSCWII.ORG is potentially down. Please contact us.", 
+            "package_type": "dol", 
+            "rating": "", 
+            "release_date": 1557464400, 
+            "shop_title_id": "", 
+            "shop_title_version": "", 
+            "short_description": "Please contact us.", 
+            "updated": 1557464400, 
+            "version": "1", 
+            "zip_size": 0, 
+            "zip_url": "https://hbb1.oscwii.org/hbb/offline/offline.zip"
+        }]''')
 
     return json.loads(json_req.text)
 
 
 # Get long description from an app's meta XML
 def long_description(app_name, repo="hbb1.oscwii.org"):
-
     try:
         xml = requests.get("https://" + repo + "/unzipped_apps/" + app_name + "/apps/" + app_name + "/meta.xml").text
     except requests.exceptions.SSLError:
