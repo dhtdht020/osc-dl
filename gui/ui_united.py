4a92584eeaae0d5533506ccb65d4d7c4c7d3a1a6
dhtdht020 <dhtdht020@gmail.com>
2022-10-29 19:08:47 +0300

    Removed unused control from main GUI

--- gui/ui_united.py
+++ gui/ui_united.py
@@ -3,7 +3,7 @@
 ################################################################################
 ## Form generated from reading UI file 'united.ui'
 ##
-## Created by: Qt User Interface Compiler version 6.3.1
+## Created by: Qt User Interface Compiler version 6.4.0
 ##
 ## WARNING! All changes made in this file will be lost when recompiling UI file!
 ################################################################################
@@ -463,7 +463,6 @@ class Ui_MainWindow(object):
         self.menuDebug.addAction(self.actionEnable_Log_File)
         self.menuDebug.addAction(self.actionClear_Log)
         self.menuDebug.addSeparator()
-        self.menuDebug.addAction(self.actionClose_the_shop)
         self.menuDebug.addAction(self.menuExperimental.menuAction())
         self.menuExperimental.addAction(self.menuAnnouncement_Banner.menuAction())
         self.menuExperimental.addAction(self.actionSelect_Theme)
@@ -472,7 +471,6 @@ class Ui_MainWindow(object):
         self.menuClients.addSeparator()
         self.menuClients.addAction(self.actionCheck_for_Updates)
         self.menuClients.addAction(self.actionRefresh)
-        self.menuHomebrew_Browser.addAction(self.actionDownload_HBB_Client_Latest)
 
         self.retranslateUi(MainWindow)
 
@@ -550,6 +548,8 @@ class Ui_MainWindow(object):
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><meta charset=\"utf-8\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "hr { height: 1px; border-width: 0; }\n"
+"li.unchecked::marker { content: \"\\2610\"; }\n"
+"li.checked::marker { content: \"\\2612\"; }\n"
 "</style></head><body style=\" font-family:'Segoe UI'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><br /></p></body></html>", None))
         self.longDescriptionLoadingSpinner.setText("")
