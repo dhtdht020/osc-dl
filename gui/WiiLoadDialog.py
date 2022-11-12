e336b9b91a8d8dc2e96f80974bce27656577ca4a
dhtdht020 <dhtdht020@gmail.com>
2022-11-12 12:59:26 +0200

    Design changes to sending dialog, code style tweaks.

--- gui/WiiLoadDialog.py
+++ gui/WiiLoadDialog.py
@@ -7,54 +7,55 @@ from PySide6.QtWidgets import QDialog, QMessageBox
 import gui_helpers
 import wiiload
 from gui import ui_WiiLoadDialog
-from utils import resource_path, file_size
+from utils import resource_path
 import serial
 import serial.tools.list_ports
 
-class WiiLoadDialog(ui_WiiLoadDialog.Ui_Dialog,QDialog):
+
+class WiiLoadDialog(ui_WiiLoadDialog.Ui_Dialog, QDialog):
     def __init__(self, package, parent=None):
         super().__init__(parent)
         self.setupUi(self)
-        
+
         self.setWindowIcon(QIcon(resource_path("assets/gui/icons/downloadlocationdialog.png")))
         self.USBDes.setText('Select the serial port for the USB Gecko adapter.<br>'
-                                'The selected app will be sent through the USBGecko to your Wii.<br><br>'
-                                f'<b>App to send: {package["display_name"]}</b><br><br>'
-                                'Make sure the USB Gecko device is attached to Slot B.<br>'
-                                'It may appear as /dev/cu.usbserial-GECKUSB0 or COM# depending on your system.<br><br>'
-                                '<b>If the selection below is not blank, your USB Gecko is the selected device.</b><br><br>')
-                                
+                            'The selected app will be sent through the USBGecko to your Wii.<br><br>'
+                            f'<b>App to send: {package["display_name"]}</b><br><br>'
+                            'Make sure the USB Gecko device is attached to Slot B.<br>'
+                            'It may appear as /dev/cu.usbserial-GECKUSB0 or COM# depending on your system.<br><br>'
+                            '<b>If the selection below is not blank, your USB Gecko is the selected device.</b>')
+
         self.USBDes.setTextFormat(Qt.TextFormat.RichText)
-        
+
         self.IPDes.setText('Enter the IP address of your Wii.<br>'
-                                'The selected app will be sent through the network to your Wii.<br><br>'
-                                f'<b>App to send: {package["display_name"]}</b><br><br>'
-                                'To find your Wii\'s IP address:<br>'
-                                '1) Enter the Homebrew Channel.<br>'
-                                '2) Press the home button on the Wii Remote.<br>'
-                                '3) Copy the IP address written in the top left corner.<br><br>')
+                           'The selected app will be sent through the network to your Wii.<br><br>'
+                           f'<b>App to send: {package["display_name"]}</b><br><br>'
+                           'To find your Wii\'s IP address:<br>'
+                           '1) Enter the Homebrew Channel.<br>'
+                           '2) Press the home button on the Wii Remote.<br>'
+                           '3) Copy the IP address written in the top left corner.')
         self.IPDes.setTextFormat(Qt.TextFormat.RichText)
-        
-        self.USBGeckoVIDPID = (0x403, 0x6001) #USBGecko: VID 0x43, PID 0x6001 
+
+        self.USBGeckoVIDPID = (0x403, 0x6001)  # USBGecko: VID 0x43, PID 0x6001
         self.PortBox.addItem("")
         self.PortBoxSerial = []
-        self.PortBoxSerial.append((None,None))
+        self.PortBoxSerial.append((None, None))
 
         # Check if USBGecko is plugged in, and ready.
         for x in serial.tools.list_ports.comports():
             self.PortBox.addItem(x.device)
-            self.PortBoxSerial.append((x.vid,x.pid))
-        
-        if self.USBGeckoVIDPID in self.PortBoxSerial: 
+            self.PortBoxSerial.append((x.vid, x.pid))
+
+        if self.USBGeckoVIDPID in self.PortBoxSerial:
             self.PortBox.setCurrentIndex(self.PortBoxSerial.index(self.USBGeckoVIDPID))
-        
+
         self.IPBox.insert(gui_helpers.settings.value("sendtowii/address"))
-        if gui_helpers.settings.value("sendtowii/previousTab") == None:
-            gui_helpers.settings.setValue("sendtowii/previousTab", 0) 
+        if gui_helpers.settings.value("sendtowii/previousTab") is None:
+            gui_helpers.settings.setValue("sendtowii/previousTab", 0)
             gui_helpers.settings.sync()
-            
+
         self.Tab.setCurrentIndex(int(gui_helpers.settings.value("sendtowii/previousTab")))
-       
+
         self.screen = QGuiApplication.primaryScreen()
         self.package = package
         self.selection = None
@@ -63,30 +64,29 @@ class WiiLoadDialog(ui_WiiLoadDialog.Ui_Dialog,QDialog):
 
         self.modeSelect = None
         self.address = None
-    
+
     def accept(self):
         self.modeSelect = currTab = self.Tab.currentIndex()
-        
-        if (currTab == 0):
+
+        if currTab == 0:
             gui_helpers.settings.setValue("sendtowii/address", self.IPBox.text())
             self.address = self.IPBox.text()
 
             if wiiload.validate_ip_regex(self.address) is None:
                 logging.warning('Invalid IP Address: ' + self.address)
                 QMessageBox.warning(self, 'Invalid IP Address', 'This IP address is invalid.')
-                return            
+                return
         else:
             self.address = self.PortBox.currentText()
             if self.address is None or self.address == "":
                 logging.warning('Invalid device')
                 QMessageBox.warning(self, 'Invalid device', 'Please select a vaild device.')
                 return
-            
 
-        gui_helpers.settings.setValue("sendtowii/previousTab", currTab) 
+        gui_helpers.settings.setValue("sendtowii/previousTab", currTab)
         gui_helpers.settings.sync()
         super().accept()
-    
+
     def reject(self):
         super().reject()
 
@@ -94,12 +94,10 @@ class WiiLoadDialog(ui_WiiLoadDialog.Ui_Dialog,QDialog):
         self.PortBox.clear()
         self.PortBoxSerial = []
         self.PortBox.addItem("")
-        self.PortBoxSerial.append((None,None))
+        self.PortBoxSerial.append((None, None))
         for x in serial.tools.list_ports.comports():
             self.PortBox.addItem(x.device)
-            self.PortBoxSerial.append((x.vid,x.pid))
+            self.PortBoxSerial.append((x.vid, x.pid))
         if self.USBGeckoVIDPID in self.PortBoxSerial:
             self.PortBox.setCurrentIndex(self.PortBoxSerial.index(self.USBGeckoVIDPID))
         super().update()
-    
-        
--- gui/ui_WiiLoadDialog.py
+++ gui/ui_WiiLoadDialog.py
@@ -16,48 +16,87 @@ from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
     QImage, QKeySequence, QLinearGradient, QPainter,
     QPalette, QPixmap, QRadialGradient, QTransform)
 from PySide6.QtWidgets import (QAbstractButton, QApplication, QComboBox, QDialog,
-    QDialogButtonBox, QLabel, QLineEdit, QPushButton,
-    QSizePolicy, QTabWidget, QVBoxLayout, QWidget)
+    QDialogButtonBox, QFrame, QHBoxLayout, QLabel,
+    QLineEdit, QPushButton, QSizePolicy, QTabWidget,
+    QVBoxLayout, QWidget)
 
 class Ui_Dialog(object):
     def setupUi(self, Dialog):
         if not Dialog.objectName():
             Dialog.setObjectName(u"Dialog")
-        Dialog.resize(562, 427)
-        Dialog.setMinimumSize(QSize(562, 427))
-        Dialog.setMaximumSize(QSize(562, 427))
+        Dialog.resize(484, 274)
         self.verticalLayout = QVBoxLayout(Dialog)
         self.verticalLayout.setObjectName(u"verticalLayout")
         self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.Tab = QTabWidget(Dialog)
         self.Tab.setObjectName(u"Tab")
+        self.Tab.setFocusPolicy(Qt.ClickFocus)
         self.tab = QWidget()
         self.tab.setObjectName(u"tab")
-        self.label_2 = QLabel(self.tab)
-        self.label_2.setObjectName(u"label_2")
-        self.label_2.setGeometry(QRect(80, 280, 191, 31))
+        self.verticalLayout_3 = QVBoxLayout(self.tab)
+        self.verticalLayout_3.setObjectName(u"verticalLayout_3")
         self.IPDes = QLabel(self.tab)
         self.IPDes.setObjectName(u"IPDes")
-        self.IPDes.setGeometry(QRect(10, 10, 511, 251))
-        self.IPBox = QLineEdit(self.tab)
+
+        self.verticalLayout_3.addWidget(self.IPDes)
+
+        self.frame = QFrame(self.tab)
+        self.frame.setObjectName(u"frame")
+        self.horizontalLayout = QHBoxLayout(self.frame)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.horizontalLayout.setContentsMargins(0, 0, 0, 0)
+        self.label_2 = QLabel(self.frame)
+        self.label_2.setObjectName(u"label_2")
+
+        self.horizontalLayout.addWidget(self.label_2)
+
+        self.IPBox = QLineEdit(self.frame)
         self.IPBox.setObjectName(u"IPBox")
-        self.IPBox.setGeometry(QRect(270, 280, 181, 31))
+
+        self.horizontalLayout.addWidget(self.IPBox)
+
+
+        self.verticalLayout_3.addWidget(self.frame)
+
         self.Tab.addTab(self.tab, "")
         self.tab_2 = QWidget()
         self.tab_2.setObjectName(u"tab_2")
+        self.verticalLayout_4 = QVBoxLayout(self.tab_2)
+        self.verticalLayout_4.setObjectName(u"verticalLayout_4")
         self.USBDes = QLabel(self.tab_2)
         self.USBDes.setObjectName(u"USBDes")
-        self.USBDes.setGeometry(QRect(10, 10, 511, 251))
-        self.label_4 = QLabel(self.tab_2)
+
+        self.verticalLayout_4.addWidget(self.USBDes)
+
+        self.frame_2 = QFrame(self.tab_2)
+        self.frame_2.setObjectName(u"frame_2")
+        self.horizontalLayout_2 = QHBoxLayout(self.frame_2)
+        self.horizontalLayout_2.setObjectName(u"horizontalLayout_2")
+        self.horizontalLayout_2.setContentsMargins(0, 0, 0, 0)
+        self.label_4 = QLabel(self.frame_2)
         self.label_4.setObjectName(u"label_4")
-        self.label_4.setGeometry(QRect(70, 290, 91, 16))
-        self.PortBox = QComboBox(self.tab_2)
+
+        self.horizontalLayout_2.addWidget(self.label_4)
+
+        self.PortBox = QComboBox(self.frame_2)
         self.PortBox.setObjectName(u"PortBox")
-        self.PortBox.setGeometry(QRect(150, 280, 241, 41))
-        self.RefreshBTN = QPushButton(self.tab_2)
+        sizePolicy = QSizePolicy(QSizePolicy.Expanding, QSizePolicy.Fixed)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.PortBox.sizePolicy().hasHeightForWidth())
+        self.PortBox.setSizePolicy(sizePolicy)
+
+        self.horizontalLayout_2.addWidget(self.PortBox)
+
+        self.RefreshBTN = QPushButton(self.frame_2)
         self.RefreshBTN.setObjectName(u"RefreshBTN")
-        self.RefreshBTN.setGeometry(QRect(400, 280, 91, 41))
+
+        self.horizontalLayout_2.addWidget(self.RefreshBTN)
+
+
+        self.verticalLayout_4.addWidget(self.frame_2)
+
         self.Tab.addTab(self.tab_2, "")
 
         self.verticalLayout_2.addWidget(self.Tab)
@@ -77,18 +116,26 @@ class Ui_Dialog(object):
         self.buttonBox.rejected.connect(Dialog.reject)
         self.RefreshBTN.clicked.connect(Dialog.update)
 
-        self.Tab.setCurrentIndex(0)
-
-
         QMetaObject.connectSlotsByName(Dialog)
     # setupUi
 
     def retranslateUi(self, Dialog):
         Dialog.setWindowTitle(QCoreApplication.translate("Dialog", u"Wii", None))
+        self.IPDes.setText(QCoreApplication.translate("Dialog", u"Enter the IP address of your Wii.<br>\n"
+"The selected app will be sent through the network to your Wii.<br><br>\n"
+"<b>App to send:</b><br><br>\n"
+"To find your Wii's IP address:<br>\n"
+"1) Enter the Homebrew Channel.<br>\n"
+"2) Press the home button on the Wii Remote.<br>\n"
+"3) Copy the IP address written in the top left corner.", None))
         self.label_2.setText(QCoreApplication.translate("Dialog", u"IP address (e.g. 192.168.1...):", None))
-        self.IPDes.setText(QCoreApplication.translate("Dialog", u"TextLabel", None))
         self.Tab.setTabText(self.Tab.indexOf(self.tab), QCoreApplication.translate("Dialog", u"Network", None))
-        self.USBDes.setText(QCoreApplication.translate("Dialog", u"TextLabel", None))
+        self.USBDes.setText(QCoreApplication.translate("Dialog", u"Select the serial port for the USB Gecko adapter.<br>\n"
+"The selected app will be sent through the USBGecko to your Wii.<br><br>\n"
+"<b>App to send:</b><br><br>\n"
+"Make sure the USB Gecko device is attached to Slot B.<br>\n"
+"It may appear as /dev/cu.usbserial-GECKUSB0 or COM# depending on your system.<br><br>\n"
+"<b>If the selection below is not blank, your USB Gecko is the selected device.</b>", None))
         self.label_4.setText(QCoreApplication.translate("Dialog", u"Device port:", None))
         self.RefreshBTN.setText(QCoreApplication.translate("Dialog", u"Refresh", None))
         self.Tab.setTabText(self.Tab.indexOf(self.tab_2), QCoreApplication.translate("Dialog", u"USB Gecko", None))
