7af5c24c9d8cd40aea59f5d412fb30a03eaf6472
dhtdht020 <dhtdht020@gmail.com>
2023-04-07 12:33:28 +0300

    Resources are now loaded from program directory rather than from current directory

--- utils.py
+++ utils.py
@@ -31,7 +31,7 @@ def app_has_extra_directories(package):
 def resource_path(relative_path):
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS, relative_path)
-    return os.path.join(os.path.abspath("."), relative_path)
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)), relative_path)
 
 
 # Returns readable file size from file length
